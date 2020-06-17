086694f0d220e7442664171548aafe46be5a0130
Lacerda53 <guilhermeandrade2013@gmail.com>
2020-06-17 09:08:43 -0300

    Subindo projeto pronto

---
+++ ApiAutentication/ApiAutentication/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using ApiAutentication.Models;
+using ApiAutentication.Repositories;
+using ApiAutentication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiAutentication.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User item)
+        {
+            var user = UserRepository.Get(item.UserName, item.Password);
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ ApiAutentication/ApiAutentication/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiAutentication.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiAutentication.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { UserName = "lacerda", Password = "lacerda", Role = "admin" });
+            return users.Where(x => x.UserName.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
